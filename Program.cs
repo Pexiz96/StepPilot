fbabdeaf863aa59c5b9084736ef98376b25bf8b3
Pexiz96 <c.thiede@icloud.com>
2026-08-21 08:18:02 +0200

    Register public holiday compliance service

--- Program.cs
+++ Program.cs
@@ -69,6 +69,7 @@ builder.Services.AddAuthorization(options =>
 builder.Services.AddScoped<CurrentUserService>();
 builder.Services.AddScoped<TenantGuard>();
 builder.Services.AddScoped<EmployeeService>();
+builder.Services.AddScoped<GermanPublicHolidayService>();
 builder.Services.AddScoped<WorkTimeComplianceService>();
 builder.Services.AddScoped<PlanningService>();
 builder.Services.AddScoped<DashboardService>();
