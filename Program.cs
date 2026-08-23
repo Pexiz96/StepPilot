e80ade450abfd2297421c89241fbff12ab05d02a
Pexiz96 <c.thiede@icloud.com>
2026-08-23 19:48:12 +0200

    Register import history service

--- Program.cs
+++ Program.cs
@@ -71,6 +71,7 @@ builder.Services.AddScoped<TenantGuard>();
 builder.Services.AddScoped<EmployeeService>();
 builder.Services.AddScoped<EmployeeImportFileService>();
 builder.Services.AddScoped<EmployeeImportService>();
+builder.Services.AddScoped<EmployeeImportHistoryService>();
 builder.Services.AddScoped<GermanPublicHolidayService>();
 builder.Services.AddScoped<WorkTimeComplianceService>();
 builder.Services.AddScoped<PlanningService>();
